fbee5615a2a71313ebbcb77092a683a013972833
Kevin Pita <gitkevin@pm.me>
2026-09-03 17:14:57 +0200

    feat: add show-me skill

--- flake.nix
+++ flake.nix
@@ -166,6 +166,7 @@
                   grill-me/SKILL.md \
                   grill-with-docs/SKILL.md \
                   handoff/SKILL.md \
+                  show-me/SKILL.md \
                   writing-for-agents/SKILL-MECHANICS.md \
                   writing-for-agents/SKILL.md
                 do
